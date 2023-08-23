8008caf3a8d448fecb49c213de648932aa439890
jaurges <antonin.david44@gmail.com>
2023-08-23 21:10:05 +0200

    finition du système de tirage

--- test/12.py
+++ test/12.py
@@ -1,4 +1,6 @@
-dict_club = {"club1"}
-n = 0
-for i in dict_club:
-        n = n + len(dict_club[i])
+n = 19
+i = 10
+if not n//2 + 1 == i and n != 1 and n!=0:
+    print("nice")
+else:
+    print("pas nice")
