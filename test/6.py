467af41d5492e2935cc5ffa54044d1c373e07590
jaurges <antonin.david44@gmail.com>
2023-08-29 20:15:32 +0200

    database_handler finito pipo

--- test/6.py
+++ test/6.py
@@ -2,8 +2,7 @@ import sys
 sys.path.append(".")
 from data.database_handler import DatabaseHandler
 
-var = DatabaseHandler("databasev2.db").return_actual_dir()
-ls = []
-for row in var:
-    ls.append(row[0])
-print(ls)
+database_handler = DatabaseHandler("databasev2.db")
+output = database_handler.return_actual_dir()
+
+print(output)
