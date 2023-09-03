7c1c6414673bbd22ed26560a87d2f8a71c29b828
jaurges <antonin.david44@gmail.com>
2023-09-03 20:46:57 +0200

    en route pour retirer des équipes das le dicto

--- gui/team_registering_gui.py
+++ gui/team_registering_gui.py
@@ -270,6 +270,7 @@ class SecondTab(QtWidgets.QWidget):
 
         self.ls_row = []
         self.dicto_team_real = {}
+        self.dicto_team_num = {}
         self.j = 0
         
 
@@ -318,22 +319,47 @@ class SecondTab(QtWidgets.QWidget):
     def set_team_forreal(self, col):
         x = self.spinbox2.value()
         y = self.lineEdit.text()
+        z = 0
         max_num_club = 0
-        print(self.dicto_team_real)
         try:
             if not self.dicto_team_real[y]:
                 pass
             else:
-                max_num_club = len(self.dicto_team_real[y])
+                try:
+                    self.dicto_team_num.setdefault(y, []).append(x)
+                    print(self.dicto_team_num[y][-1])
+                    print(f"{self.dicto_team_num[y][-2]}\n")
+                    diff = self.dicto_team_num[y][-1] - self.dicto_team_num[y][-2]
+                    #print(diff)
+                    max_num_club = len(self.dicto_team_real[y])
+                    n = 0
+                    z = max_num_club
+                    if diff<0:
+                        print("feur")
+                        for i in range(abs(diff)):
+                            #self.dicto_team_real[y][-i] = [int, int, ""]
+                            del self.dicto_team_real[y].popitem()
+                            self.j = self.j -1
+                            n = n+1
+                        self.value_automat.emit(self.dicto_team_real)
+                    else: 
+                        max_num_club = len(self.dicto_team_real[y])
+                        #print(max_num_club)
+                except Exception as e: 
+                    max_num_club = len(self.dicto_team_real[y])
+                    #print(e)
         except KeyError:
             pass
-        
         n = 0
-        for i in range(x):
+        z = max_num_club
+        for _ in range(z, x):
             self.dicto_team_real.setdefault(y, []).append([n+max_num_club, col, f"team{self.j+1}"])
             self.j = self.j +1
             n = n+1
         self.value_automat.emit(self.dicto_team_real)
+        print(self.dicto_team_real)
+        
+        
 
 
 class SettingCombo(QtWidgets.QDialog):
--- test/testdelinfini.py
+++ test/testdelinfini.py
@@ -1,58 +1,37 @@
-import re
-liste_de_chaines = ["Il y a 42 pommes.", "Le prix est de 3.14 euros.", "123 est un nombre.", "Pas de nombre ici."]
-liste_de_nombres = []
-pattern = r'\d+\.\d+|\d+'
-
-for chaine in liste_de_chaines:
-    correspondances = re.findall(pattern, chaine)
-    for correspondance in correspondances:
-        nombre = float(correspondance)
-        liste_de_nombres.append(nombre)
-
-print(liste_de_nombres)
-
-
 def set_team_forreal(self, col):
-        num4 = 0
-        num3 = 0
-        num2 = 0
-        pattern = r'\d+\.\d+|\d+'
-        ls = ["0"]
-        
-        num = int
-        if self.dicto_team_real:
-            for i in self.dicto_team_real:
-                for chaine in i:
-                    correspondences = re.findall(pattern, chaine)
-                    for correspondence in correspondences:
-                        num = correspondence
-                        ls.append(num)
+    x = self.spinbox2.value()
+    y = self.lineEdit.text()
+    z = 0
+    max_num_club = 0
+    try:
+        if not self.dicto_team_real[y]:
+            n = 0
+            for _ in range(x):
+                self.dicto_team_real.setdefault(y, []).append([n, col, f"team{self.j+1}"])
+                self.dicto_team_num.setdefault(y, []).append(x)
+                self.j = self.j +1
+                n = n+1
+            self.value_automat.emit(self.dicto_team_real)
         else:
-            pass
-        #ls = [float(item) for item in ls]
-        num4 = max(ls)
-        num3 = int(num4)
-        if num3 == 0:
-            pass
-        else:
-            num3 = num3-1
-        num2 = self.spinbox2.value()+ num3
-        for i in range(num3, num2):
-            try:
-                try :
-                    for k in self.dicto_team_real[self.lineEdit.text()][2]:
-                     
-                        if k == f"team{i+1}":
-                            pass
-                        else:
-                            self.dicto_team_real.setdefault(self.lineEdit.text(), []).append([self.j, col, f"team{i+1}"])
-                except IndexError:
-                    self.dicto_team_real.setdefault(self.lineEdit.text(), []).append([self.j, col, f"team{i+1}"])
-            except KeyError:
-                self.dicto_team_real.setdefault(self.lineEdit.text(), []).append([self.j, col, f"team{i+1}"])
+            diff = self.dicto_team_num[y][-1] - self.dicto_team_num[y][-2]
+            if diff<0:
+                for i in range(abs(diff)):
+                    self.dicto_team_real[y][-i] = []
+                self.value_automat.emit(self.dicto_team_real)
+            else: 
+                n = 0
+                z = max_num_club
+                for _ in range(z, x):
+                    self.dicto_team_real.setdefault(y, []).append([n+max_num_club, col, f"team{self.j+1}"])
+                    self.dicto_team_num.setdefault(y, []).append(x)
+                    self.j = self.j +1
+                    n = n+1
+                self.value_automat.emit(self.dicto_team_real)
+    except KeyError:
+        n = 0
+        for _ in range(x):
+            self.dicto_team_real.setdefault(y, []).append([n, col, f"team{self.j+1}"])
+            self.dicto_team_num.setdefault(y, []).append(x)
             self.j = self.j +1
-            #print(self.dicto_team_real)
-        print(self.dicto_team_real)
+            n = n+1
         self.value_automat.emit(self.dicto_team_real)
-
-    
